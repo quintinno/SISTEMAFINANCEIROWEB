ee1476b5b7eee24fc33c7a1724ff1b66609a53f2
Desenvolvimento <repository.desenvolvimento@gmail.com>
2021-05-25 00:06:18 -0300

    SISTEMAFINANCEIROAPI-38112503202118001
    - Implementação da Funcionalidade - Manter Despesa
    - Implementação de Cadastro de Despesa Fixa

---
+++ src/app/model/despesa-fixa-model.ts
@@ -0,0 +1,9 @@
+export class DespesaFixaModel {
+    codigo: number;
+    categoriaDespesa: any;
+    dayaVencimento: Date;
+    dataPagamento: Date;
+    valorPagamento: number;
+    fontePagamento: any;
+    tipoSituacaoPagamento: any;
+}
--- src/app/page/gerenciador-despesa-fixa/despesa-fixa-cadastrar/despesa-fixa-cadastrar.component.ts
+++ src/app/page/gerenciador-despesa-fixa/despesa-fixa-cadastrar/despesa-fixa-cadastrar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ContratoModel } from 'src/app/model/contrato-model';
+import { GerenciadorContratoService } from 'src/app/service/gerenciador-contrato.service';
 
 @Component({
   selector: 'app-despesa-fixa-cadastrar',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DespesaFixaCadastrarComponent implements OnInit {
 
-  constructor() { }
+  public contratoList = new Array;
+
+  public contratoModel: ContratoModel = new ContratoModel();
+
+  public contratoCodigo = null;
+
+  constructor(
+    private gerenciadorContratoService: GerenciadorContratoService,
+  ) { }
 
   ngOnInit(): void {
+    this.recuperarContrato();
+  }
+
+  public cadastrar() {
+    console.log("Cadastrar Despesa Fixa...");
+  }
+
+  public recuperarContrato() {
+    this.gerenciadorContratoService.recuperarContratoList().subscribe(response => {
+      response.forEach( ( resultado ) => {
+        if(resultado.identificador != null && resultado.tipoContrato.codigo != 41) {
+          resultado.identificador = resultado.pessoaContratado.nome + " (" + resultado.identificador + ")";
+          this.contratoList.push(resultado);
+        }
+      });
+    });
+  }
+
+  // TODO - Gerar data de vencimento de acordo com o mes corrente
+  public recuperarVencimento() {
+    this.contratoList.forEach( ( contratoRetorno ) => {
+      if(Number(this.contratoCodigo) === contratoRetorno.codigo) {
+        this.contratoModel = contratoRetorno;
+        return this.contratoModel.dataInicioVigencia;
+      }
+    });
+  }
+
+  // TODO - Recuperar valor cadastrado no Contrato
+  public recuperarValor() {
+
   }
 
 }
