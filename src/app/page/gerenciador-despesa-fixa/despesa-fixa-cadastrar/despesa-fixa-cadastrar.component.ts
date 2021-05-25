29bd9d94d371c93a4c6dfc22cf32eb2a91ef4dbe
Desenvolvimento <repository.desenvolvimento@gmail.com>
2021-05-25 14:50:21 -0300

    SISTEMAFINANCEIRO-38112503202118001
    - Implementação da Funcionalidade - Manter Despesa
    - Implementação de Cadastro de Despesa Fixa

--- src/app/page/gerenciador-despesa-fixa/despesa-fixa-cadastrar/despesa-fixa-cadastrar.component.ts
+++ src/app/page/gerenciador-despesa-fixa/despesa-fixa-cadastrar/despesa-fixa-cadastrar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ContratoModel } from 'src/app/model/contrato-model';
+import { DespesaModel } from 'src/app/model/despesa-model';
+import { GerenciadorContaBancariaService } from 'src/app/service/gerenciador-conta-bancaria.service';
 import { GerenciadorContratoService } from 'src/app/service/gerenciador-contrato.service';
 
 @Component({
@@ -9,22 +12,29 @@ import { GerenciadorContratoService } from 'src/app/service/gerenciador-contrato
 })
 export class DespesaFixaCadastrarComponent implements OnInit {
 
-  public contratoList = new Array;
-
   public contratoModel: ContratoModel = new ContratoModel();
+  public despesaModel: DespesaModel = new DespesaModel();
+
+  public contratoList = new Array;  
+  public contaBancariaList = new Array;
 
   public contratoCodigo = null;
+  public isCampoDataPagamentoInvalido = false;
 
   constructor(
+    private router: Router,
     private gerenciadorContratoService: GerenciadorContratoService,
+    private gerenciadorContaBancariaService: GerenciadorContaBancariaService
   ) { }
 
   ngOnInit(): void {
     this.recuperarContrato();
+    this.recuperarFontePagamento();
   }
 
   public cadastrar() {
     console.log("Cadastrar Despesa Fixa...");
+    this.validarCamposFormulario();
   }
 
   public recuperarContrato() {
@@ -48,9 +58,45 @@ export class DespesaFixaCadastrarComponent implements OnInit {
     });
   }
 
-  // TODO - Recuperar valor cadastrado no Contrato
-  public recuperarValor() {
+  /**
+	 	  Recuperar os dados da seguinte forma:
+	 	
+          Banco do Brasil - Conta Corrente (0826717460)
+          Banco do Brasil - Conta Salário (0826717460)
+          Banco do Brasil - Conta Poupança (0826717460)
+          Banco do Brasil - Cartão de Crédito (4854-6441-3151-0095)
+          Banco do Brasil - Cartão de Débito (4854-6441-3151-0095)
 
+          Banco Santader - Conta Corrente (3441010833661)
+          Banco Santader - Conta Poupança (3441010833661)
+          Banco Santader - Cartão de Crédito (5447-3154-3418-1741)
+          Banco Santader - Cartão de Débito (5447-3154-3418-1741)
+
+          Banco Nubank - Conta Corrente (000010)
+          Banco Nubank - Cartão de Crédito (5162-9204-3768-0921)
+          Banco Nubank - Cartão de Débito (5162-9204-3768-0921)
+
+          Banco Caixa Econômica - Conta Corrente (0655013000406876)
+          Banco Caixa Econômica - Cartão de Poupança (603689-0010-63446-9068)
+
+        Retorna o model "Conta Bancaria"
+	 */
+  public recuperarFontePagamento() {
+    this.gerenciadorContaBancariaService.recuperarContaBancariaList().subscribe((response) => {
+      response.forEach((retorno) => {
+        // console.log(retorno);
+        console.log(retorno.pessoaInstituicaoFinanceira.nome + " (" + retorno.tipoContaBancaria.descricao + ")");
+      })
+      this.contaBancariaList.push(response);
+    });
+  }
+
+  public validarCamposFormulario() {
+    if(this.contratoModel.dataInicioVigencia == null) this.isCampoDataPagamentoInvalido = true;
+  }
+
+  public redirecionarPaginaMonitoramentoDespesa() {
+      this.router.navigate(["/despesa-monitoramento"]);
   }
 
 }
