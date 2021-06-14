6fc11af18d122fdbedabe0fbdafe4e183adf7fc0
Desenvolvimento <repository.desenvolvimento@gmail.com>
2021-06-14 14:43:13 -0300

    SISTEMAFINANCEIRO-14062021135738

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ const express = require("express");
 const path = require("path");
 const application = express();
 
-const PORT = process.env.PORT || 8080;
+application.use(express.static("./dist/SISTEMAFINANCEIROWEB"));
 
-application.use(express.static(__dirname + "/dist/SISTEMAFINANCEIROWEB"));
+application.get("/*", (req, response) =>
+    response.sendFile("index.html", {root: "dist/SISTEMAFINANCEIROWEB/"}),
+);
 
-application.get("/*", (request, response) => {
-	response.sendFile(__dirname + "/dist/SISTEMAFINANCEIROWEB");
-});
-
-application.listen(PORT, () => {
-    console.log("Sistema Financeiro Excutando [" + PORT + "]");
-});
+application.listen(process.env.PORT || 8080);
