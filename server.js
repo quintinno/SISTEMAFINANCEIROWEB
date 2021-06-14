4c4a1dd5ec27a041010cf07b403f0a59469584b6
Desenvolvimento <repository.desenvolvimento@gmail.com>
2021-06-14 14:27:38 -0300

    SISTEMAFINANCEIRO-14062021135738

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 
 application.use(express.static(__dirname + "/dist/SISTEMAFINANCEIROWEB"));
 
-application.get("/", (request, response) => {
+application.get("/*", (request, response) => {
 	response.sendFile(__dirname + "/dist/SISTEMAFINANCEIROWEB");
 });
 
