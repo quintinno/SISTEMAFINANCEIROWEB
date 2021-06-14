9dafbc6dd7720a995c5917ad5ddbf58d95b010ec
Desenvolvimento <repository.desenvolvimento@gmail.com>
2021-06-14 13:59:02 -0300

    SISTEMAFINANCEIRO-14062021135738
    - Implantação do projeto no ambiente de Homologação no Heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const application = express();
+
+const PORT = process.env.PORT || 8080;
+
+application.use(express.static(__dirname + "/dist/SISTEMAFINANCEIROWEB"));
+
+application.get("/", (request, response) => {
+	response.sendFile(__dirname + "/dist/SISTEMAFINANCEIROWEB");
+});
+
+application.listen(PORT, () => {
+    console.log("Sistema Financeiro Excutando [" + PORT + "]");
+});
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  url_base_api_homologacao: "https://sistemafinanceiroapi.herokuapp.com",
 };
