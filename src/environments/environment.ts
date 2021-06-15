84ed0c280b2eeb628e81a04d1600c3a21542ad8e
José Quintino <jose.quintino@fornecedores.sicoob.com.br>
2021-06-15 16:16:21 -0300

    v1.0.0-SNAPSHOT

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   url_base_api_desenvolvimento: "http://localhost:9090",
   url_base_api_teste: "https://sistemafinanceiroapi.herokuapp.com",
-  url_base_api_homologacao: "https://sistemafinanceiroapi.herokuapp.com",
+  url_base_api_homologacao: "https://sistemafinanceiroapip.herokuapp.com",
 };
